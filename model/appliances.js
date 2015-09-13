ceeaba7967d52a301f8dceef15fb44c4b1936b1b
raidersofthenight <ianemailaddress@gmail.com>
2015-09-12 20:08:38 -0700

    add dependencies to app.js, build router

--- model/appliances.js
+++ model/appliances.js
@@ -1,7 +1,32 @@
 var mongoose = require('mongoose');
 
-var applianceSchema = new mongoose.Schema({
+var dishwasherSchema = new mongoose.Schema({
+	brand_name: String;
 	model_name: String;
 	model_number: String;
+	type: String;
+	gallons_per_cycle: String;
+	energy_factor: String;
+	kwh_per_year: String;
+});
 
-})
+var clotheswasherSchema = new mongoose.Schema({
+	brand_name: String;
+	model_name: String;
+	model_number: String;
+	volume_cubic_feet: String;
+	meets_most_efficient_criteria: boolean;
+});
+
+var displaysSchema = new mongoose.Schema({
+	brand_name: String;
+	model_name: String;
+	model_number: String;
+	screen_size: String;
+	off_mode_pwr_watts: String;
+	on_mode_pwr_watts: String;
+	sleep_mode_pwr_watts: String;
+	panel_tech: String;
+	backlight_tech: String;
+	resolution: String;
+});
---
+++ routes/appliances.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
